bbb867d3dd86ab68753116a7744bf5f5955ae580
duonugget <duonghuy335@gmail.com>
2025-01-10 12:16:50 +0700

    adding new components

---
+++ src/components/ClassroomPage/Exercise/ExerciseDetail/ExerciseDetail.js
@@ -0,0 +1,343 @@
+import React, {useState, useEffect} from "react";
+
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from "axios";
+
+import { saveAs } from 'file-saver';
+
+import { PiFileSql, PiFilePdf, PiFileZip, PiFileDoc, PiFileImage, PiFileText, PiFile } from "react-icons/pi";
+
+import { BsFiletypeXlsx, BsFiletypeXls } from "react-icons/bs";
+
+import FileUploader from "../../Attached_files/Attached_files";
+
+import { BsThreeDotsVertical } from "react-icons/bs";
+
+ function File_container({ file_name, file_url }) {
+    const iconMap = {
+        pdf: <PiFilePdf size={35} color="black" />,
+        doc: <PiFileDoc size={35} color="black" />,
+        docx: <PiFileDoc size={35} color="black" />,
+        xls: <BsFiletypeXls size={35} color="black" />,
+        xlsx: <BsFiletypeXlsx size={35} color="black" />,
+        jpg: <PiFileImage size={35} color="black" />,
+        png: <PiFileImage size={35} color="black" />,
+        txt: <PiFileText size={35} color="black" />,
+        zip: <PiFileZip size={35} color="black" />,
+        sql: <PiFileSql size={35} color="black" />,
+        default: <PiFile size={35} color="black" />,
+    };
+
+    const getFileIcon = (fileName) => {
+        const extension = fileName.split('.').pop().toLowerCase();
+        return iconMap[extension] || iconMap['default'];
+    };
+    const handleDownload = () => {
+        saveAs(file_url, file_name);
+    };
+
+    return (
+        <div className="file-container" style={{ cursor: "pointer" }}>
+            <div className="file-icon">
+                {getFileIcon(file_name)}
+            </div>
+            <div className="file-name">
+                <a
+                    href={file_url} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    onClick={handleDownload}
+                    style={{ color: "black" }}
+                    onMouseEnter={(e) => e.target.style.textDecoration = "underline"}
+                    onMouseLeave={(e) => e.target.style.textDecoration = "none"}
+                >   
+                    {file_name}
+                </a>
+            </div>
+        </div>
+    );
+}
+
+function Exercise_description() {
+    const navigate = useNavigate(); 
+    const { classId, assignmentId } = useParams();
+
+    const [files, setFiles] = useState([]);
+    const handleFileChange = (newFiles) => {
+        setFiles(newFiles); 
+    };
+
+    const [updatedData, setUpdatedData] = useState({
+        exercise_name: "",
+        date: null,
+        exercise_note: "",
+        files: [],
+    });
+
+    const [assignmentData, setAssignmentData] = useState({
+        exercise_name: "",
+        date: null,
+        exercise_note: "",
+        files: [],
+    });
+
+    const [isEditing, setIsEditing] = useState(false);
+    const handleEditClick = () => {
+        setIsEditing(!isEditing);
+        setUpdatedData({ ...assignmentData });
+    };
+
+    const handleTextChange = (e) => {
+        const { name, value } = e.target;
+        setUpdatedData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleFileDelete = (index) => {
+        setUpdatedData((prevData) => ({
+            ...prevData,
+            files: prevData.files.filter((_, i) => i !== index),
+        }));
+    };
+
+    const handleUpdate = async() => {
+        try {
+            setLoading(true);
+
+            // Create FormData object for multipart/form-data
+            const formData = new FormData();
+            formData.append("title", updatedData.exercise_name || "");
+            formData.append("descriptions", updatedData.exercise_note || "");
+
+            for (let i = 0; i < files.length; i++) {
+                formData.append('additional_attachments', files[i]);
+            }
+            const removalFiles = assignmentData.files.filter(
+                (file) => !updatedData.files.some((updatedFile) => updatedFile === file)
+            );
+    
+            removalFiles.forEach((file, index) => {
+                formData.append(`removal_attachments`, file.filename);
+            });
+
+            console.log("FormData Content:", Array.from(formData.entries()));
+
+            // Send the PATCH request
+            const response = await axios.put(
+                `http://localhost:8000/classroom/${classId}/assignment/${assignmentId}/update`,
+                formData,
+                {
+                    params: {
+                        token: sessionStorage.getItem('access_token'),
+                    },
+                }
+            );
+            console.log("updated success!")
+        } catch (error) {
+            console.error("Error fetching post details:", error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const handleDelete = async() => {
+        try {
+            setLoading(true);
+            const response = await axios.delete(
+                `http://localhost:8000/classroom/${classId}/assignment/${assignmentId}/delete`,
+                {
+                    params: {
+                        token: sessionStorage.getItem('access_token'),
+                    },
+                }
+            );
+            console.log("Deleted success!")
+        } catch (error) {
+            console.error("Error fetching assignment details:", error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const [isOption, setOption] = useState(false);
+    const handleOption = () => {
+        setOption(!isOption);
+    }
+
+    const [loading, setLoading] = useState(true);
+
+    const onBack = () => {
+        navigate(`/c/${classId}/a`);
+    };
+    
+    useEffect(() => {
+        const fetchAssignmentDetails = async () => {
+            try {
+                const response = await axios.get(
+                    `http://localhost:8000/classroom/${classId}/assignment/${assignmentId}/detail`,
+                    {
+                        params: {
+                            token: sessionStorage.getItem('access_token'),
+                        },
+                    }
+                );
+                const data = response.data;
+                setAssignmentData({
+                    exercise_name: data.title,
+                    date: new Date(data.updated_at),
+                    exercise_note: data.descriptions,
+                    files: data.attachments,
+                });
+            } catch (error) {
+                console.error("Error fetching assignment details:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchAssignmentDetails();
+    }, [classId, assignmentId]);
+
+    if (loading) {
+        return (
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "60vh",
+                }}
+            >
+                <div style={{ fontSize: "1.5rem", fontWeight: "bold", color: "#333" }}>
+                    Loading...
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="exercise-overall">
+            <div className="exercise-description-container">
+                <div className="exercise_description">
+                    <div>
+                        <div className="button-wrapper">
+                            <button className="return-button" onClick={onBack}>
+                                Back to Assignments
+                            </button>
+                        </div>
+                        <div className="exercise-header">
+                            {isEditing ? (
+                                <input
+                                    type="text"
+                                    name="exercise_name"
+                                    style={{
+                                        backgroundColor: "transparent",
+                                        border: "2px solid #ccc",
+                                        borderRadius: "4px",
+                                        padding: "8px",
+                                        width: "100%",
+                                        fontSize: "1rem",
+                                    }}
+                                    defaultValue={assignmentData.exercise_name}
+                                    onChange={handleTextChange}
+                                />
+                            ) : (
+                                <p>{assignmentData.exercise_name}</p>
+                            )}
+                            <pre>
+                                {assignmentData.date
+                                    ? `Due date: ${assignmentData.date.toLocaleDateString()} ${assignmentData.date.toLocaleTimeString()}`
+                                    : "Loading ..."}
+                            </pre>
+                            <div>
+                            <BsThreeDotsVertical 
+                                onClick={handleOption} 
+                                style={{
+                                    cursor: "pointer",
+                                }}
+                            />
+                            {isOption && (
+                                <div className="option-buttons">
+                                    <button 
+                                        onClick={handleEditClick} 
+                                        style={{ 
+                                            cursor: "pointer"
+                                        }}
+                                    >
+                                        Update
+                                    </button>
+                                    <button 
+                                        onClick={handleDelete} 
+                                        style={{ 
+                                            cursor: "pointer"
+                                        }}
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            )}
+                            </div>
+                        </div>
+                        <div className="exercise-content">
+                            {isEditing ? (
+                                <>
+                                <textarea
+                                    name="exercise_note"
+                                    rows={5}
+                                    defaultValue={assignmentData.exercise_note
+                                    }
+                                    style={{
+                                        backgroundColor: "transparent",
+                                        border: "2px solid #ccc",
+                                        borderRadius: "4px",
+                                        padding: "8px",
+                                        width: "100%",
+                                        fontSize: "1rem",
+                                    }}
+                                    onChange={handleTextChange}
+                                />
+                                </>
+                            ) : (
+                                <p>{assignmentData.exercise_note}</p>
+                            )}
+                        </div>
+                    </div>
+                    <div className="files-grid-container">
+                    <div className="files-grid">
+                        {(isEditing ? updatedData.files : assignmentData.files).map((file, index) => (
+                            <div
+                                key={index}
+                                style={{ display: "flex", alignItems: "center", gap: "10px" }}
+                            >
+                                <File_container file_name={file.filename} file_url={file.url} />
+                                {isEditing && (
+                                    <button
+                                        onClick={() => handleFileDelete(index)}
+                                        style={{
+                                            backgroundColor: "transparent",
+                                            border: "none",
+                                            color: "red",
+                                            fontWeight: "bold",
+                                            cursor: "pointer",
+                                            fontSize: "18px",
+                                        }}
+                                    >
+                                        X
+                                    </button>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div className="file-uploader-container">
+                <FileUploader files={files} setFiles={handleFileChange} sendHandle={handleUpdate} />
+            </div>
+        </div>
+    );
+}
+
+export default Exercise_description;
---
+++ src/components/ClassroomPage/posts/Posts.js
@@ -0,0 +1,276 @@
+import ChildHeader from "../ChildHeader";
+import FileUploader from "../Attached_files/Attached_files";
+
+import "../Exercise/Exercise.css"
+
+import React, { Suspense, useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useOutlet, Outlet, useParams } from 'react-router-dom';
+import { FadeLoader } from "react-spinners";
+
+import 'filepond/dist/filepond.min.css';
+
+function PostForm({handleClick}) {
+    const [files, setFiles] = useState([]);
+    const { classId } = useParams();
+    const [loading, setLoading] = useState(false);
+    const [notificationForm, setNotificationForm] = useState({
+        title: "",
+        descriptions: "",
+        // class_id: classId,
+        start_at: new Date(),
+        end_at: new Date(),
+        attachments: [],
+    });
+    
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNotificationForm((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleFileChange = (newFiles) => {
+        setFiles(newFiles);  // Update files state
+        setNotificationForm((prevData) => ({
+            ...prevData,
+            attachments: newFiles,  // Update form with new files array
+        }));
+    };
+    
+    const handleUpload = () => {
+        setLoading(true);
+        console.log(notificationForm);
+        console.log(notificationForm.attachments); // Check if the form data is correct
+        // Create FormData object
+        const formData = new FormData();
+        formData.append('title', notificationForm.title);
+        formData.append('content', notificationForm.descriptions);
+
+        // Append files to FormData
+        for (let i = 0; i < notificationForm.attachments.length; i++) {
+            formData.append('attachments', notificationForm.attachments[i]);
+        }
+
+        // Sending the request with FormData
+        axios.post(
+            `http://localhost:8000/classroom/${classId}/post/create`,
+            formData,
+            {
+                params: {
+                    token: sessionStorage.getItem('access_token'),
+                },
+            }
+        )
+            .then(response => {
+                console.log('Noti created successfully');
+                setLoading(false);
+            })
+            .catch(error => {
+                alert('Error uploading data');
+                console.error(error);
+                setLoading(false);
+            });
+    };
+    if (loading) {
+        return(
+            <>
+            <div className="login-loading">
+                <FadeLoader
+                color="#ffb800"
+                height={50}
+                margin={60}
+                radius={3}
+                width={15}
+                />
+            </div>
+            </>
+        )
+    }
+
+    return (
+        <div className="newClassDiv">
+          <div style={{ backgroundColor: "#F4A481", textAlign: "center" }}>
+            <h2 style={{ justifySelf: "center" }}>Create post</h2>
+          </div>
+          <div className="informationDiv">
+            <h2 className="informationTitle">Informations</h2>
+          </div>
+          <div className="informationForm">
+            <label>
+              Post name
+              <textarea 
+               name="title"
+               rows="1" 
+               style={{ resize: "none"}}
+               onChange={handleChange}
+               placeholder="null"></textarea>
+            </label>
+            <label>
+              Post descriptions
+              <textarea
+                name="descriptions"
+                placeholder="washedup n_g_a ngo"
+                rows="5" 
+                onChange={handleChange}
+                style={{ resize: "vertical" }}
+                ></textarea>
+            </label>
+          </div>
+          <div className="file-uploader">
+            <FileUploader 
+                files = {files}
+                setFiles={handleFileChange}
+                sendHandle={handleUpload}
+            />
+            </div>
+          <div className="buttonContainer">
+            <button className="cancelButton" onClick={handleClick}>
+              Cancel
+            </button>
+            <button className="createButton" onClick={handleUpload}>
+              Upload post
+            </button>
+          </div>
+        </div>
+      );
+}
+
+function AddPost() {
+    const [showDiv, setShowDiv] = useState(false);
+    const handleClick = () => {
+      setShowDiv(!showDiv);
+    };
+    return (
+      <div className="createClassContainer">
+        <button className="buttonWrapper" onClick={handleClick}>
+          <span> + Tạo post</span>
+        </button>
+        {showDiv && <PostForm handleClick={handleClick}/>}
+      </div>
+    );
+}
+
+function Notification({ author, date, content, onClick }) {
+    return (
+        <div className="notification-box" >
+            <div className="notification-header" onClick={onClick}>
+                <span className="notification-author">{author}</span>
+                <span className="notification-date">
+                    <span style={{ marginRight: '15px' }}>{date.toLocaleDateString()}</span>
+                    <span>{date.toLocaleTimeString()}</span>
+                </span>
+            </div>
+            <div className="notification-content">{content}</div>
+        </div>
+    );
+}
+
+function Notifications({ notifications, onNotificationClick }) {
+    return (
+        <div className="notifications-container">
+            <div className="notifications-list">
+                {notifications.map((notification, index) => (
+                    <Notification
+                        key={index}
+                        author={notification.author}
+                        date={notification.date}
+                        content={notification.content}
+                        onClick={() => onNotificationClick(notification.postId)}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+function Post() {
+    const { classId } = useParams();
+    const navigate = useNavigate();
+    const outlet = useOutlet();
+    const [posts, setPosts] = useState([]);
+    const [selectedPost, setSelectedPost] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchNotifications = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8000/classroom/${classId}/post/all`,
+                    { params: {
+                                token: sessionStorage.getItem("access_token"),
+                            },
+                    }
+                );
+                const data = response.data;
+                const postContent = data.map((post) => ({
+                    author: post.author,
+                    date: new Date(post.updated_at),
+                    content: post.title,
+                    postId: post.id,
+                }));
+                setPosts(postContent);
+            } catch (Error) {
+                console.error("Error fetching notifications:", Error);
+                setError(Error)
+            } finally{
+                setLoading(false);
+            }
+        };
+
+        if (classId) {
+            fetchNotifications();
+        }
+    
+    }, [classId, setError]);
+
+    const handleNotificationClick = async (postId) => {
+        navigate(`/c/${classId}/p/${postId}`);
+    };
+
+    const handleBackClick = () => {
+        setPosts(null);
+    };
+
+    if (loading) {
+        return(
+            <>
+            <div className="login-loading">
+                <FadeLoader
+                color="#ffb800"
+                height={50}
+                margin={60}
+                radius={3}
+                width={15}
+                />
+            </div>
+            </>
+        )
+    }
+
+    if (error) {
+        return(
+        <>        
+            <div>{error.message}</div>
+            <AddPost/>
+        </>
+        );
+    }
+
+    return (
+        <div>
+            <ChildHeader nameHeader={"Posts"} />
+            <div>
+                <Notifications
+                    notifications={posts}
+                    onNotificationClick={handleNotificationClick}
+                />
+                <Outlet />
+            </div>
+            <AddPost/>
+        </div>
+    );
+}
+
+export default Post;
