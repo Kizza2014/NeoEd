596907dd38401024cd55e0be9f78517051e459f7
Kizza2014 <hoangkimgiap_t67@hus.edu.vn>
2025-01-11 09:29:06 +0700

    add classroom invitation code, remove class room password

--- src/test.py
+++ src/test.py
@@ -1,8 +1,26 @@
-from datetime import datetime
-import pytz
+import uuid
+import zlib
 
-timezone = pytz.timezone('Asia/Ho_Chi_Minh')
+BASE62_ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-current_time_gmt7 = datetime.now(timezone)
+def encode_base62(number, alphabet=BASE62_ALPHABET):
+    base = len(alphabet)
+    encoded = []
+    while number:
+        number, rem = divmod(number, base)
+        encoded.append(alphabet[rem])
+    return ''.join(reversed(encoded))
 
-print("Current time in GMT+7:", current_time_gmt7)
+def crc32_encode(unique_id):
+    # Compute CRC32 checksum of the UUID
+    checksum = zlib.crc32(unique_id.bytes) & 0xffffffff
+    return encode_base62(checksum)
+
+class_id = 'classroom-b620573c-7c3f-43cf-956a-62b667811394'
+unique_id = uuid.UUID('-'.join(class_id.split('-')[1:]))
+
+# Encode
+encoded_id = crc32_encode(unique_id)
+
+print("Original UUID:", unique_id)
+print("CRC32 Encoded ID:", encoded_id)
